141ec9a0dbfdf5ede8f1f567b18c4773151a745a
Kane <84501314+kanetan4@users.noreply.github.com>
2023-02-01 10:14:15 +0800

    Delete tutorial2.R

--- tutorial2.R
+++
@@ -1,6 +0,0 @@
-library(data.table)
-setwd('/Users/kane/Desktop/NTU BCG/Year 2 Sem 2/BC2407 Analytics II/BC2407 Course Materials/S2 Review of Basic Analytics')
-
-flat = fread('resale-flat-prices-2019.csv')
-flat$flat_type = factor(flat$flat_type)
-
